3a43f4a09841772c80ba74ed0e691acbe6ac88e9
Owain Williams <owain@andersdigital.com>
2018-06-21 15:55:44 +0100

    init commit

---
+++ SizeCompare/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SizeCompare.cpp
+ *
+ * Created: 21/06/2018 15:47:36
+ * Author : owain
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
